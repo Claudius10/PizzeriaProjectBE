fc04467f753509263f2e54214689fdb07b65774a
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2023-07-02 23:34:26 +0200

    same as last

--- src/main/java/PizzaApp/api/utility/auth/CookieUtils.java
+++ src/main/java/PizzaApp/api/utility/auth/CookieUtils.java
@@ -66,7 +66,7 @@ public final class CookieUtils {
 		Cookie[] cookies = request.getCookies();
 		if (cookies != null)
 			for (Cookie cookie : cookies) {
-				cookie.setDomain(".up.railway.app");
+				cookie.setDomain("https://pizzeriaproject-production.up.railway.app");
 				cookie.setValue("");
 				cookie.setPath("/");
 				cookie.setMaxAge(0);

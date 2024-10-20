33d7609a2a1ad349522d67d85d6e2dff842c9c6f
claudio <66148715+Claudius10@users.noreply.github.com>
2024-10-20 19:47:10 +0200

    First commit PizzeriaAngularFE

--- src/main/resources/dbdata/address_202407141210.sql
+++ src/main/resources/dbdata/address_202407141210.sql
@@ -1,3 +1,3 @@
-INSERT INTO pizzeriaproject.address (door,floor,gate,staircase,street,street_nr) VALUES
-	 (NULL,NULL,NULL,NULL,'Calle Alustre',15),
-	 (NULL,NULL,NULL,NULL,'Avenida Viciosa',221);
+INSERT INTO pizzeria.address (door, floor, gate, staircase, street, street_nr, id)
+VALUES (NULL, NULL, NULL, NULL, 'Calle Alustre', 15, 1),
+       (NULL, NULL, NULL, NULL, 'Avenida Viciosa', 221, 2);
--- src/main/resources/dbdata/store_202407141210.sql
+++ src/main/resources/dbdata/store_202407141210.sql
@@ -1,3 +1,3 @@
-INSERT INTO pizzeriaproject.store (name,phone_number,schedule,address_id) VALUES
-	 ('Alustre',666555666,'L-D 12:00h-00:00h',1),
-	 ('Viciosa',555666555,'L-D 12:00h-00:00h',2);
+INSERT INTO pizzeria.store (name, phone_number, schedule, address_id, id)
+VALUES ('Alustre', 666555666, 'L-D 12:00h-00:00h', 1, 1),
+       ('Viciosa', 555666555, 'L-D 12:00h-00:00h', 2, 2);

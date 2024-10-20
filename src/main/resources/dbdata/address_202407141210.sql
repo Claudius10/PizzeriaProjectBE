d33e437ce2ba81249eeb13c0fec49150889d2e8f
claudio <66148715+Claudius10@users.noreply.github.com>
2024-10-20 21:18:48 +0200

    Refactor

--- src/main/resources/dbdata/address_202407141210.sql
+++ src/main/resources/dbdata/address_202407141210.sql
@@ -1,3 +1,5 @@
 INSERT INTO pizzeria.address (door, floor, gate, staircase, street, street_nr, id)
-VALUES (NULL, NULL, NULL, NULL, 'Calle Alustre', 15, 1),
-       (NULL, NULL, NULL, NULL, 'Avenida Viciosa', 221, 2);
+VALUES (NULL, NULL, NULL, NULL, 'Calle Alustre', 15, 1);
+
+INSERT INTO pizzeria.address (door, floor, gate, staircase, street, street_nr, id)
+VALUES (NULL, NULL, NULL, NULL, 'Avenida Viciosa', 221, 2);
